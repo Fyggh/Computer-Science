a6a3f851316fc2e451f8f07ab9323885fbd751a7
200503041 <200503041@INV506707.lbusd.local>
2017-09-11 12:34:11 -0700

    Define the number FIFTY_FOUR and add print statement

--- src/io/github/fyggh/Main.java
+++ src/io/github/fyggh/Main.java
@@ -1,10 +1,23 @@
 package io.github.fyggh;
 
 public class Main {
+	
+	/**
+	 * Number defined as one greater than FIFTY_THREE
+	 */
+	private final static int FIFTY_FOUR = 54;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 
+		System.out.println(GET_FIFTY_FOUR());
 
+	}
+
+	/**
+	 * @return the number defined as one greater than FIFTY_THREE
+	 */
+	public final static int GET_FIFTY_FOUR() {
+		return FIFTY_FOUR;
 	}
 
 }
