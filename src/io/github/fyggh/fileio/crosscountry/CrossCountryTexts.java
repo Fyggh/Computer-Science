f824aeae0e1c334b3d22a85e002e1de86f6b63c0
Fyggh <t.fleming.ca@gmail.com>
2017-12-04 23:39:07 -0800

    Create fileio stuff. kinda useless

---
+++ src/io/github/fyggh/fileio/crosscountry/CrossCountryTexts.java
@@ -0,0 +1,22 @@
+package io.github.fyggh.fileio.crosscountry;
+
+import java.util.HashMap;
+
+public class CrossCountryTexts {
+
+	public static void main(String[] args) {
+		
+		String template = "Hey, %s!  I know you're probably very busy, but it's time to work a bit on the slideshow. If you have a free half an hour in the next few days, could you go to the LBPXC shutterfly site (https://longbeachpolyboysxc.shutterfly.com/, password: lbpxc) and send me around 20 pictures from %s? I'd suggest downloading them to your computer, making a .zip archive out of them, and sending the archive to sir.fleming.ca@gmail.com";
+		
+		HashMap<String, String> seniorsToRace = new HashMap<String, String>() {{
+			put("Aidan Johnson", "Roughrider");
+			put("Bryce Barbee", "Reno");
+			put("Bryan Espinoza", "Signal Hill");
+			put("Antonio Jorgenson", "Footlocker");
+			put("Griffin Roach", )
+		}};
+		
+
+	}
+
+}
